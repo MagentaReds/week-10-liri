6a6fe791fdc6b3ea48959a5581574721a9e59303
MagentaReds <grantodaniels@gmail.com>
2017-03-28 18:16:19 -0500

    Added file

---
+++ keys.js
@@ -0,0 +1,17 @@
+// UTBC
+// 	Consumer Key (API Key)	p1y1U7gEi9ChCRy2GzmsVLWtd
+// 	Consumer Secret (API Secret)	Ajp7VMF69Y3RjBgKhimA3jHXx3Ht7OQq6GPtm4xd3hbzf5lQPm
+	
+// 	Access Token	846783386760986625-MJYmiE71rj7NjwX8Jvg7OZ8PYcQ301d
+// 	Access Token Secret	BPnqPzyeuzOg7KamvibZ1xh06yhcKatAtpHy3k3DcQCop
+
+//These keys are for a dummy twitter account.
+
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: "p1y1U7gEi9ChCRy2GzmsVLWtd",
+  consumer_secret: "Ajp7VMF69Y3RjBgKhimA3jHXx3Ht7OQq6GPtm4xd3hbzf5lQPm",
+  access_token_key: "846783386760986625-MJYmiE71rj7NjwX8Jvg7OZ8PYcQ301d",
+  access_token_secret: "BPnqPzyeuzOg7KamvibZ1xh06yhcKatAtpHy3k3DcQCop"
+};
